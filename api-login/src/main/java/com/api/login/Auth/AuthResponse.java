80a79f300a2d5dab859d36864d281b9cd7938db1
Pablo Abal Diez <pabloabaldiez@gmail.com>
2023-10-02 18:32:44 -0300

    first commit

---
+++ api-login/src/main/java/com/api/login/Auth/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.api.login.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    String token; 
+}
---
+++ api-login/src/main/java/com/api/login/User/Role.java
@@ -0,0 +1,6 @@
+package com.api.login.User;
+
+public enum Role {
+    ADMIN,
+    USER  
+}
